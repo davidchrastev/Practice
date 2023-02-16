aa890ec379457e9a8ade6742f7e1401cffba46b9
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-02-16 18:57:37 +0200

    HackerRank

---
+++ src/HackerRank/BetweenTwoSets.java
@@ -0,0 +1,36 @@
+package HackerRank;
+
+import java.util.List;
+
+public class BetweenTwoSets {
+
+    public static int getTotalX(List<Integer> a, List<Integer> b) {
+        int count = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            boolean factorA = true;
+            boolean factorB = true;
+
+            for (Integer num : a) {
+                if (i % num != 0) {
+                    factorA = false;
+                    break;
+                }
+            }
+
+            for (Integer num : b) {
+                if (i % num != 0) {
+                    factorB = false;
+                    break;
+                }
+            }
+
+            if (factorA && factorB) {
+                count++;
+            }
+
+        }
+        return count;
+
+    }
+}
