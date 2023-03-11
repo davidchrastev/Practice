40a514006a7b8f457c1cb4ea7ac09b351c142dbd
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-03-11 19:08:08 +0200

    interesting problem to solve i need to upgrade it for now i works but it's not fine

--- src/HackerRank/BetweenTwoSets.java
+++ src/HackerRank/BetweenTwoSets.java
@@ -6,38 +6,40 @@ import java.util.List;
 public class BetweenTwoSets {
 
     public static int getTotalX(List<Integer> a, List<Integer> b) {
-        int count = 0;
+//        int count = 0;
+//
+//        for (int i = 1; i <= 100; i++) {
+//            boolean factorA = true;
+//            boolean factorB = true;
+//
+//            for (Integer num : a) {
+//                if (i % num != 0) {
+//                    factorA = false;
+//                    break;
+//                }
+//            }
+//
+//            for (Integer num : b) {
+//                if (i % num != 0) {
+//                    factorB = false;
+//                    break;
+//                }
+//            }
+//
+//            if (factorA && factorB) {
+//                count++;
+//            }
+//
+//            Map<Worker, Double> workers = new HashMap<>();
+//
+//            worker.entrySet()
+//                    .stream()
+//                    sorted
+//
+//        }
+//        return count;
 
-        for (int i = 1; i <= 100; i++) {
-            boolean factorA = true;
-            boolean factorB = true;
-
-            for (Integer num : a) {
-                if (i % num != 0) {
-                    factorA = false;
-                    break;
-                }
-            }
-
-            for (Integer num : b) {
-                if (i % num != 0) {
-                    factorB = false;
-                    break;
-                }
-            }
-
-            if (factorA && factorB) {
-                count++;
-            }
-
-            Map<Worker, Double> workers = new HashMap<>();
-
-            worker.entrySet()
-                    .stream()
-                    sorted
-
-        }
-        return count;
+        return 1;
 
     }
 }
