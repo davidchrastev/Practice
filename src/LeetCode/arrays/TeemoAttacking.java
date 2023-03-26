f62696404b40ca820d1ad43f7e261faf858d362a
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-03-26 20:48:35 +0300

    Daily problem solve.

--- src/LeetCode/arrays/TeemoAttacking.java
+++ src/LeetCode/arrays/TeemoAttacking.java
@@ -8,6 +8,13 @@ public class TeemoAttacking {
         int totalPoisonedTime = 0;
         int previousAttackEndTime = -1;
 
+        totalPoisonedTime = getTotalPoisonedTime(timeSeries, duration, totalPoisonedTime, previousAttackEndTime);
+
+        return totalPoisonedTime;
+
+    }
+
+    private static int getTotalPoisonedTime(int[] timeSeries, int duration, int totalPoisonedTime, int previousAttackEndTime) {
         for (int attackTime : timeSeries) {
             int attackEndTime = attackTime + duration - 1;
 
@@ -19,8 +26,6 @@ public class TeemoAttacking {
             totalPoisonedTime += poisonTime;
             previousAttackEndTime = attackEndTime;
         }
-
         return totalPoisonedTime;
-
     }
 }
