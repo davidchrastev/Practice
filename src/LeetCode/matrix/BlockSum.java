2a08956978553959da06362b2d2eecb4993de93c
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-03-29 22:19:31 +0300

    not finding a solution

---
+++ src/LeetCode/matrix/BlockSum.java
@@ -0,0 +1,12 @@
+package LeetCode.matrix;
+
+public class BlockSum {
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] toReturn = new int[m][n];
+
+        return toReturn;
+
+    }
+}
