f4b4936b5346eaaffe2d4df3c87d1bd9c0389448
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-04-09 21:01:21 +0300

    Fast way to find duplicates

---
+++ src/LeetCode/Data_Structures_1/Duplicate.java
@@ -0,0 +1,21 @@
+package LeetCode.Data_Structures_1;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Duplicate {
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        Arrays.stream(nums)
+                .forEach(e -> System.out.println(set.add(e)));
+
+        return set.size() != nums.length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1, 2, 3, 1}));
+    }
+}
