670d7b495afd3701b1f76f6fc5f95ad72a0fe243
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-04-13 00:23:30 +0300

    Daily challenges

--- src/LeetCode/Data_Structures_1/PascalTriangle.java
+++ src/LeetCode/Data_Structures_1/PascalTriangle.java
@@ -1,7 +1,29 @@
 package LeetCode.Data_Structures_1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PascalTriangle {
     public static void main(String[] args) {
 
     }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    int left = result.get(i - 1).get(j - 1);
+                    int right = result.get(i - 1).get(j);
+                    row.add(left + right);
+                }
+            }
+            result.add(row);
+        }
+        return result;
+
+    }
+
 }
