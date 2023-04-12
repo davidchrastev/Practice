827c97f1c4b204e84b147844661b3f4cd6a48333
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-04-12 23:44:22 +0300

    Daily challenges

---
+++ src/LeetCode/Data_Structures_1/PascalTriangle.java
@@ -0,0 +1,7 @@
+package LeetCode.Data_Structures_1;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/LeetCode/Data_Structures_1/Reshape.java
@@ -0,0 +1,8 @@
+package LeetCode.Data_Structures_1;
+
+public class Reshape {
+    public static void main(String[] args) {
+
+    }
+
+}
