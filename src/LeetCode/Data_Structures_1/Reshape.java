26f323ccfec880347eb0774e5a144e3e062e8eb3
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-04-12 23:59:40 +0300

    Daily challenges

--- src/LeetCode/Data_Structures_1/Reshape.java
+++ src/LeetCode/Data_Structures_1/Reshape.java
@@ -1,8 +1,31 @@
 package LeetCode.Data_Structures_1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Reshape {
     public static void main(String[] args) {
-
+        System.out.println(matrixReshape(new int[][]{{1, 2}, {3, 4}}, 1, 4));
     }
+    public static int[][] matrixReshape(int[][] mat, int r, int c) {
+        List<Integer> resize = new ArrayList<>();
+        int[][] newMatrix = new int[r][c];
+
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat[row].length; col++) {
+                resize.add(mat[row][col]);
+            }
+        }
+        int counter = 0;
+        for (int row = 0; row < r; row++) {
+            for (int col = 0; col < c; col++) {
+                newMatrix[row][col] = resize.get(counter);
+                counter++;
+            }
+        }
+
+        return newMatrix;
+    }
+
 
 }
