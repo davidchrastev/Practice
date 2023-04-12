6ef0fef2965614a8316736727d9f6ba8eef60ad6
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-04-13 00:21:43 +0300

    Daily challenges

--- src/LeetCode/Data_Structures_1/Reshape.java
+++ src/LeetCode/Data_Structures_1/Reshape.java
@@ -6,25 +6,40 @@ import java.util.List;
 public class Reshape {
     public static void main(String[] args) {
         System.out.println(matrixReshape(new int[][]{{1, 2}, {3, 4}}, 1, 4));
+        int[][] matrix = matrixReshape(new int[][]{{1, 2}, {3, 4}}, 1, 4);
+        print(matrix);
     }
     public static int[][] matrixReshape(int[][] mat, int r, int c) {
-        List<Integer> resize = new ArrayList<>();
+        int countRow = mat.length;
+        int countCols = mat[0].length;
+        if (countRow * countCols != r * c) {
+            return mat;
+        }
         int[][] newMatrix = new int[r][c];
+        int rowCounter = 0;
+        int colCounter = 0;
 
-        for (int row = 0; row < mat.length; row++) {
-            for (int col = 0; col < mat[row].length; col++) {
-                resize.add(mat[row][col]);
+        for (int[] row : mat) {
+
+            for (int num : row) {
+                newMatrix[rowCounter][colCounter] = num;
+                colCounter++;
+                if (colCounter == c) {
+                    colCounter = 0;
+                    rowCounter++;
+                }
             }
         }
-        int counter = 0;
-        for (int row = 0; row < r; row++) {
-            for (int col = 0; col < c; col++) {
-                newMatrix[row][col] = resize.get(counter);
-                counter++;
-            }
-        }
-
         return newMatrix;
+    }
+
+    public static void print(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
     }
 
 
