b2e7a9f7037d94b0b659f308b721ab87bbe45263
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-04-16 17:22:27 +0300

    Daily challenge solve

---
+++ src/LeetCode/Data_Structures_1/ReverseList.java
@@ -0,0 +1,15 @@
+package LeetCode.Data_Structures_1;
+
+public class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
