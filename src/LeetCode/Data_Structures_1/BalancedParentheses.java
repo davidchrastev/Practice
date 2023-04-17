06bceaa6510c3cea17a62ef2aba8c511ce776717
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-04-17 21:12:42 +0300

    Daily challenge solve

---
+++ src/LeetCode/Data_Structures_1/BalancedParentheses.java
@@ -0,0 +1,24 @@
+package LeetCode.Data_Structures_1;
+
+import java.util.Stack;
+
+public class BalancedParentheses {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
