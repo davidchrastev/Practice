8f456479a71bcc8c306ebcf11550f1aa2fc7e5ef
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-04-19 17:54:05 +0300

    Daily challenge solve

---
+++ src/LeetCode/Data_Structures_1/PostOrder.java
@@ -0,0 +1,29 @@
+package LeetCode.Data_Structures_1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class PostOrder {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(0, node.val);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return result;
+
+    }
+
+}
