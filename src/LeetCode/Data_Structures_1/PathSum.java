44960649d9bb02cd58ae1c63b40edb47ee3f0bf0
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-04-20 18:41:43 +0300

    Daily challenge solve

--- src/LeetCode/Data_Structures_1/PathSum.java
+++ src/LeetCode/Data_Structures_1/PathSum.java
@@ -2,6 +2,12 @@ package LeetCode.Data_Structures_1;
 
 public class PathSum {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return true;
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            return true;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
