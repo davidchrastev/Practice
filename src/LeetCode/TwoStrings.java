942f0cbc0d766d23de7231403c1aea49c69653ac
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-06-27 23:34:55 +0300

    practice

---
+++ src/LeetCode/TwoStrings.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+public class TwoStrings {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+
+        int haystackLength = haystack.length();
+        int needleLength = needle.length();
+
+        for (int i = 0; i <= haystackLength - needleLength; i++) {
+            int j;
+            for (j = 0; j < needleLength; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+            }
+
+            if (j == needleLength) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
