a6b30098cab0f8ec18e04e2e2c06e0934e501eeb
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-07-01 17:47:52 +0300

    Basic algorithm

---
+++ src/MaximumElementFinder.java
@@ -0,0 +1,19 @@
+public class MaximumElementFinder {
+    public static void main(String[] args) {
+        int[] array = {5, 2, 9, 1, 7, 3};
+
+        int max = getMaxEl(array, array[0]);
+
+        System.out.println("The maximum element in the array is: " + max);
+    }
+
+    private static int getMaxEl(int[] array, int max) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
+
